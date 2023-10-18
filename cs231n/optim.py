f67fc3e119272166cbcdeea20be39496f168ee21
chenghao <ch3197@qq.com>
2023-10-18 22:33:53 +0800

    全连接差最后一个写完

--- cs231n/optim.py
+++ cs231n/optim.py
@@ -69,7 +69,10 @@ def sgd_momentum(w, dw, config=None):
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    v = config['momentum'] * v - config['learning_rate'] * dw
+    w = w + v
+    next_w = w
+    '''next_w = w+v'''
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
@@ -106,8 +109,11 @@ def rmsprop(w, dw, config=None):
     # config['cache'].                                                        #
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
-    pass
+    
+    config['cache'] = config['decay_rate'] * config['cache'] + ( 1 - config['decay_rate']) * dw**2
+    w += - config['learning_rate'] * dw / (np.sqrt(config['cache']) + config['epsilon'])
+    next_w = w
+    '''next_w = w -config['learning_rate'] * dw / (np.sqrt(config['cache']) + config['epsilon'])'''
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
@@ -152,7 +158,13 @@ def adam(w, dw, config=None):
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    config['t'] += 1
+    config['m'] = config['beta1'] * config['m'] + ( 1 - config['beta1']) * dw
+    mt = config['m'] / ( 1 - config['beta1']**config['t'])
+    config['v'] = config['beta2']*config['v'] + (1-config['beta2'])*(dw**2)
+    vt = config['v'] / (1-config['beta2']**config['t'])
+    w += - config['learning_rate'] * mt / (np.sqrt(vt) + config['epsilon'])
+    next_w = w
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
