4efe534814024a486f787985a23ade0828aedc2d
Cheng Hao <ch3197@qq.com>
2023-10-26 14:47:18 +0800

    fill batchnorm_backward_alt

--- cs231n/layers.py
+++ cs231n/layers.py
@@ -321,11 +321,12 @@ def batchnorm_backward(dout, cache):
     dgamma = np.sum(dout*x_head, axis=0) # 为什么要按元素相乘？？？
 
     N = x.shape[0]
-
+    
     dx_head = dout*gamma # (N, D)
     dvar = np.sum(dx_head*(x - mean)*(-1/2)*(var + eps)**(-3/2), axis=0) # (D, )
     dmean = np.sum(dx_head*(-1)/(var + eps)**(1/2) + dvar*(-2/N)*(x - mean), axis=0) # (D, )
     dx = dx_head/(var + eps)**(1/2) + dmean/N + dvar*(2/N)*(x - mean) # (N, D)
+
     
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
@@ -359,7 +360,24 @@ def batchnorm_backward_alt(dout, cache):
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    beta = cache['beta']
+    gamma = cache['gamma']
+    x = cache['x']
+    x_head = cache['X_head']
+    mean = cache['sample_mean']
+    var = cache['sample_var']
+    eps = cache['eps']
+
+    dbeta = dout.sum(axis = 0)
+    dgamma = np.sum(dout*x_head, axis=0) # 为什么要按元素相乘？？？
+
+    N = x.shape[0]
+
+    dx_head = dout*gamma # (N, D)
+    dvar = np.sum(dx_head*(x - mean)*(-1/2)*(var + eps)**(-3/2), axis=0) # (D, )
+    dmean = np.sum(dx_head*(-1)/(var + eps)**(1/2) + dvar*(-2/N)*(x - mean), axis=0) # (D, )
+    dx = dx_head/(var + eps)**(1/2) + dmean/N + dvar*(2/N)*(x - mean) # (N, D)
+    
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
